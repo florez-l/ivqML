d130a42cf064ff52932311fdb4719e2f7b42b1a7
Leonardo Flórez-Valencia <leonardo.florez@gmail.com>
2021-09-28 20:53:21 -0500

    Some minor adjustments.

--- examples/python3/FeedForwardNeuralNetwork_02.py
+++ examples/python3/FeedForwardNeuralNetwork_02.py
@@ -10,16 +10,24 @@ import PUJ.Data.Algorithms, PUJ.Data.Normalize
 import PUJ.Model.NeuralNetwork.FeedForward
 import PUJ.Optimizer.GradientDescent
 
+def debug( J, dJ, t, i, show ):
+  if show:
+    print( i, dJ )
+  # end if
+# end def
+
 # -- Configure network
 fwd_nn = PUJ.Model.NeuralNetwork.FeedForward( )
-fwd_nn.LoadParameters( sys.argv[ 1 ] )
+fwd_nn.LoadParameters( 'D:/GitHub/PUJ_ML/examples/data/generic_2d_3_layer_fwd.nn' )
 
 # -- Data
-D = numpy.genfromtxt( sys.argv[ 2 ], delimiter = ',' )
+D = numpy.genfromtxt( 'D:/GitHub/PUJ_ML/examples/data/input_01.csv', delimiter = ',' )
 numpy.random.shuffle( D )
 X_real = D[ : , 0 : 2 ]
 y_real = D[ : , 2 : 3 ]
 X_real, X_min, X_off = PUJ.Data.Normalize.MinMax( X_real )
+
+print( X_real.shape, y_real.shape )
 
 # -- Train
 cost_tra = PUJ.Model.NeuralNetwork.FeedForward.Cost( X_real, y_real, fwd_nn )
@@ -33,7 +41,7 @@ tI, nI = PUJ.Optimizer.GradientDescent(
   max_iter = 100000,
   epsilon = 1e-8,
   debug_step = 1000,
-  debug_function = None
+  debug_function = debug
   )
 
 print( '===================================' )
--- lib/python3/PUJ/Optimizer/GradientDescent.py
+++ lib/python3/PUJ/Optimizer/GradientDescent.py
@@ -4,7 +4,8 @@
 
 import math, numpy
 
-## -------------------------------------------------------------------------
+'''
+'''
 def GradientDescent( cost, **kwargs ):
   a = 1e-1
   l = 0.0
@@ -13,8 +14,7 @@ def GradientDescent( cost, **kwargs ):
   e = 1e-8
   ds = 100
   df = None
-  n = cost.VectorSize( )
-  t = numpy.random.rand( 1, n ) * 1e-1
+  t = cost.GetInitialParameters( )
 
   if 'learning_rate' in kwargs: a = float( kwargs[ 'learning_rate' ] )
   if 'regularization' in kwargs: l = float( kwargs[ 'regularization' ] )
@@ -23,20 +23,6 @@ def GradientDescent( cost, **kwargs ):
   if 'epsilon' in kwargs: e = float( kwargs[ 'epsilon' ] )
   if 'debug_step' in kwargs: ds = int( kwargs[ 'debug_step' ] )
   if 'debug_function' in kwargs: df = kwargs[ 'debug_function' ]
-  if 'init_theta' in kwargs:
-    t0 = kwargs[ 'init_theta' ]
-    if isinstance( t0, ( int, float ) ):
-      t = numpy.ones( ( 1, n ) ) * float( t0 )
-    elif isinstance( t0, list ):
-      t = numpy.matrix( t0 )
-    elif isinstance( t0, numpy.matrix ):
-      t = t0
-    elif isinstance( t0, str ):
-      if t0 == 'none':
-        t = cost.GetInitialParameters( )
-      # end if
-    # end if
-  # end if
 
   # Init loop
   [ J, gt ] = cost.CostAndGradient( t )
@@ -77,11 +63,10 @@ def GradientDescent( cost, **kwargs ):
     i += 1
 
   # end while
+
   if df != None:
     df( J, dJ, t, i, True )
   # end if
-  
-  return ( t, i )
 # end def
 
 ## eof - $RCSfile$
