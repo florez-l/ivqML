0550c502889559c6e6398dbabaf2d2317fa3297d
Leonardo Flórez-Valencia <florez-l@javeriana.edu.co>
2021-10-27 23:43:56 -0500

    ...

--- lib/cxx/PUJ/Model/NeuralNetwork/ActivationFunction.cxx
+++ lib/cxx/PUJ/Model/NeuralNetwork/ActivationFunction.cxx
@@ -1,23 +1,26 @@
 // =========================================================================
 // @author Leonardo Florez-Valencia (florez-l@javeriana.edu.co)
 // =========================================================================
-#ifndef __PUJ__NeuralNetwork__ActivationFunction__h__
-#define __PUJ__NeuralNetwork__ActivationFunction__h__
 
-#include <PUJ/Traits.h>
+#include <PUJ/Model/NeuralNetwork/ActivationFunction.h>
 
 // -------------------------------------------------------------------------
-#define PUJ_NN_ActivationFunction_Base( _n_ ) \
-      template< class _TScalar, class _TTraits > \
-      typename PUJ::Model::NeuralNetwork::_n_< _TScalar >::TMatrix \
-      PUJ::Model::NeuralNetwork::_n_< _TScalar >:: \
-      operator()( const TMatrix& z )
+#define PUJ_NN_ActivationFunction_Base( _n_ )                           \
+  template< class _TScalar, class _TTraits >                            \
+  typename PUJ::Model::NeuralNetwork::_n_< _TScalar, _TTraits >::TMatrix \
+  PUJ::Model::NeuralNetwork::_n_< _TScalar, _TTraits >::                \
+  operator()( const TMatrix& z )
 
 // -------------------------------------------------------------------------
 PUJ_NN_ActivationFunction_Base( ArcTan )
 {
+  static const auto f = []( TScalar v ) -> TScalar
+    {
+      return( std::atan( v ) );
+    };
+  return( z.unaryExpr( f ) );
 }
-      
+
 // -------------------------------------------------------------------------
 PUJ_NN_ActivationFunction_Base( BinaryStep )
 {
@@ -31,6 +34,14 @@ PUJ_NN_ActivationFunction_Base( BinaryStep )
 // -------------------------------------------------------------------------
 PUJ_NN_ActivationFunction_Base( ELU )
 {
+  static const auto f = [=]( TScalar v ) -> TScalar
+    {
+      if( v < TScalar( 0 ) )
+        return( this->m_Alpha * ( std::exp( v ) - TScalar( 1 ) ) );
+      else
+        return( v );
+    };
+  return( z.unaryExpr( f ) );
 }
 
 // -------------------------------------------------------------------------
@@ -68,7 +79,7 @@ PUJ_NN_ActivationFunction_Base( Logistic )
 // -------------------------------------------------------------------------
 PUJ_NN_ActivationFunction_Base( RandomReLU )
 {
-  static const auto f = []( TScalar v ) -> TScalar
+  static const auto f = [=]( TScalar v ) -> TScalar
     {
       return( ( v <  TScalar( 0 ) )? this->m_Alpha * v: v );
     };
@@ -88,16 +99,177 @@ PUJ_NN_ActivationFunction_Base( ReLU )
 // -------------------------------------------------------------------------
 PUJ_NN_ActivationFunction_Base( SoftMax )
 {
+  static const auto f = []( TScalar v ) -> TScalar
+    {
+      return( std::exp( v ) );
+    };
+  auto e =  z.unaryExpr( f );
+  return( e / e.sum( ) );
 }
 
 // -------------------------------------------------------------------------
-PUJ_NN_ActivationFunction_Base( SoftMin )
+PUJ_NN_ActivationFunction_Base( SoftPlus )
 {
+  static const auto f = []( TScalar v ) -> TScalar
+    {
+      return( std::log( TScalar( 1 ) + std::exp( v ) ) );
+    };
+  return( z.unaryExpr( f ) );
 }
 
 // -------------------------------------------------------------------------
 PUJ_NN_ActivationFunction_Base( Tanh )
 {
+  static const auto f = []( TScalar v ) -> TScalar
+    {
+      return( std::tanh( v ) );
+    };
+  return( z.unaryExpr( f ) );
 }
+
+// -------------------------------------------------------------------------
+#define PUJ_NN_ActivationFunction_Derivative( _n_ )                     \
+  template< class _TScalar, class _TTraits >                            \
+  typename PUJ::Model::NeuralNetwork::_n_< _TScalar, _TTraits >::TMatrix \
+  PUJ::Model::NeuralNetwork::_n_< _TScalar, _TTraits >::                \
+  operator[]( const TMatrix& z )
+
+// -------------------------------------------------------------------------
+PUJ_NN_ActivationFunction_Derivative( ArcTan )
+{
+  static const auto f = []( TScalar v ) -> TScalar
+    {
+      return( TScalar( 1 ) / ( TScalar( 1 ) + ( v * v ) ) );
+    };
+  return( z.unaryExpr( f ) );
+}
+
+// -------------------------------------------------------------------------
+PUJ_NN_ActivationFunction_Derivative( BinaryStep )
+{
+  return( TMatrix::Zero( z.rows( ), z.cols( ) ) );
+}
+
+// -------------------------------------------------------------------------
+PUJ_NN_ActivationFunction_Derivative( ELU )
+{
+  static const auto f = [=]( TScalar v ) -> TScalar
+    {
+      if( v < TScalar( 0 ) )
+        return( this->m_Alpha * std::exp( v ) );
+      else
+        return( TScalar( 1 ) );
+    };
+  return( z.unaryExpr( f ) );
+}
+
+// -------------------------------------------------------------------------
+PUJ_NN_ActivationFunction_Derivative( Identity )
+{
+  return( TMatrix::Ones( z.rows( ), z.cols( ) ) );
+}
+
+// -------------------------------------------------------------------------
+PUJ_NN_ActivationFunction_Derivative( LeakyReLU )
+{
+  static const auto f = []( TScalar v ) -> TScalar
+    {
+      return( ( v <  TScalar( 0 ) )? TScalar( 1e-2 ): TScalar( 1 ) );
+    };
+  return( z.unaryExpr( f ) );
+}
+
+// -------------------------------------------------------------------------
+PUJ_NN_ActivationFunction_Derivative( Logistic )
+{
+  static const auto f = []( TScalar v ) -> TScalar
+    {
+      static const TScalar _0 = TScalar( 0 );
+      static const TScalar _1 = TScalar( 1 );
+      static const TScalar _bnd = TScalar( 40 );
+
+      if( -_bnd < v && v < _bnd )
+      {
+        TScalar e = _1 / ( _1 + std::exp( -v ) );
+        return( e * ( _1 - e ) );
+      }
+      else
+        return( TScalar( 0 ) );
+    };
+  return( z.unaryExpr( f ) );
+}
+
+// -------------------------------------------------------------------------
+PUJ_NN_ActivationFunction_Derivative( RandomReLU )
+{
+  static const auto f = [=]( TScalar v ) -> TScalar
+    {
+      return( ( v <  TScalar( 0 ) )? this->m_Alpha: TScalar( 1 ) );
+    };
+  return( z.unaryExpr( f ) );
+}
+
+// -------------------------------------------------------------------------
+PUJ_NN_ActivationFunction_Derivative( ReLU )
+{
+  static const auto f = []( TScalar v ) -> TScalar
+    {
+      return( ( v <  TScalar( 0 ) )? TScalar( 0 ): TScalar( 1 ) );
+    };
+  return( z.unaryExpr( f ) );
+}
+
+// -------------------------------------------------------------------------
+PUJ_NN_ActivationFunction_Derivative( SoftMax )
+{
+  /* TODO
+     static const auto f = []( TScalar v ) -> TScalar
+     {
+     return( std::exp( v ) );
+     };
+     auto e =  z.unaryExpr( f );
+     return( e / e.sum( ) );
+  */
+  return( TMatrix::Zero( z.rows( ), z.cols( ) ) );
+}
+
+// -------------------------------------------------------------------------
+PUJ_NN_ActivationFunction_Derivative( SoftPlus )
+{
+  static const auto f = []( TScalar v ) -> TScalar
+    {
+      TScalar e = std::exp( v );
+      return( e / ( e + TScalar( 1 ) ) );
+    };
+  return( z.unaryExpr( f ) );
+}
+
+// -------------------------------------------------------------------------
+PUJ_NN_ActivationFunction_Derivative( Tanh )
+{
+  static const auto f = []( TScalar v ) -> TScalar
+    {
+      TScalar t = std::tanh( v );
+      return( TScalar( 1 ) - ( t * t ) );
+    };
+  return( z.unaryExpr( f ) );
+}
+
+// -------------------------------------------------------------------------
+#define PUJ_NN_ActivationFunction_Instances( _n_ )                      \
+  template class PUJ_ML_EXPORT PUJ::Model::NeuralNetwork::_n_< float >; \
+  template class PUJ_ML_EXPORT PUJ::Model::NeuralNetwork::_n_< double >
+
+PUJ_NN_ActivationFunction_Instances( ArcTan );
+PUJ_NN_ActivationFunction_Instances( BinaryStep );
+PUJ_NN_ActivationFunction_Instances( ELU );
+PUJ_NN_ActivationFunction_Instances( Identity );
+PUJ_NN_ActivationFunction_Instances( LeakyReLU );
+PUJ_NN_ActivationFunction_Instances( Logistic );
+PUJ_NN_ActivationFunction_Instances( RandomReLU );
+PUJ_NN_ActivationFunction_Instances( ReLU );
+PUJ_NN_ActivationFunction_Instances( SoftMax );
+PUJ_NN_ActivationFunction_Instances( SoftPlus );
+PUJ_NN_ActivationFunction_Instances( Tanh );
 
 // eof - $RCSfile$
--- lib/cxx/PUJ/Model/NeuralNetwork/ActivationFunction.h
+++ lib/cxx/PUJ/Model/NeuralNetwork/ActivationFunction.h
@@ -7,41 +7,40 @@
 #include <PUJ/Traits.h>
 
 // -------------------------------------------------------------------------
-#define PUJ_NN_ActivationFunction( _n_ ) \
-      template< class _TScalar, class _TTraits = PUJ::Traits< _TScalar > > \
-      class PUJ_ML_EXPORT _n_ \
-      : public ActivationFunction< _TScalar, _TTraits > \
-      { \
-      public: \
-        using Superclass = ActivationFunction< _TScalar, _TTraits >; \
-        PUJ_TraitsMacro( _n_ ); \
-      public: \
-        _n_( ) : Superclass( ) { } \
-        virtual ~_n_( ) = default; \
-        virtual TMatrix operator()( const TMatrix& z ) override; \
-        virtual TMatrix operator[]( const TMatrix& z ) override; \
-      }
+#define PUJ_NN_ActivationFunction( _n_ )                                \
+  template< class _TScalar, class _TTraits = PUJ::Traits< _TScalar > >  \
+  class PUJ_ML_EXPORT _n_                                               \
+    : public ActivationFunction< _TScalar, _TTraits >                   \
+  {                                                                     \
+  public:                                                               \
+    using Superclass = ActivationFunction< _TScalar, _TTraits >;        \
+    PUJ_TraitsMacro( _n_ );                                             \
+  public:                                                               \
+    _n_( ) : Superclass( ) { }                                          \
+    virtual ~_n_( ) = default;                                          \
+    virtual TMatrix operator()( const TMatrix& z ) override;            \
+    virtual TMatrix operator[]( const TMatrix& z ) override;            \
+  }
 
 // -------------------------------------------------------------------------
-#define PUJ_NN_ActivationFunction( _n_, _a_, _v_ ) \
-      template< class _TScalar, class _TTraits = PUJ::Traits< _TScalar > > \
-      class PUJ_ML_EXPORT _n_ \
-      : public ActivationFunction< _TScalar, _TTraits > \
-      { \
-      public: \
-        using Superclass = ActivationFunction< _TScalar, _TTraits >; \
-        PUJ_TraitsMacro( _n_ ); \
-      public: \
-        _n_( ) : Superclass( ) { } \
-        virtual ~_n_( ) = default; \
-        void Set##_a_( ) { this->m_##_a_ = v; } \
-        const TScalar& Get##_a_( ) const { return( this->m_##_a_ ); } \
-        virtual TMatrix operator()( const TMatrix& z ) override; \
-        virtual TMatrix operator[]( const TMatrix& z ) override; \
-      protected: \
-        TScalar m_##_a_ { _v_ }; \
-      }
-
+#define PUJ_NN_ActivationFunction_Parameter( _n_, _a_, _v_ )            \
+  template< class _TScalar, class _TTraits = PUJ::Traits< _TScalar > >  \
+  class PUJ_ML_EXPORT _n_                                               \
+    : public ActivationFunction< _TScalar, _TTraits >                   \
+  {                                                                     \
+  public:                                                               \
+    using Superclass = ActivationFunction< _TScalar, _TTraits >;        \
+    PUJ_TraitsMacro( _n_ );                                             \
+  public:                                                               \
+    _n_( ) : Superclass( ) { }                                          \
+    virtual ~_n_( ) = default;                                          \
+    void Set##_a_( const TScalar& v ) { this->m_##_a_ = v; }            \
+    const TScalar& Get##_a_( ) const { return( this->m_##_a_ ); }       \
+    virtual TMatrix operator()( const TMatrix& z ) override;            \
+    virtual TMatrix operator[]( const TMatrix& z ) override;            \
+  protected:                                                            \
+    TScalar m_##_a_ { _v_ };                                            \
+  }
 
 namespace PUJ
 {
@@ -56,15 +55,15 @@ namespace PUJ
       {
       public:
         PUJ_TraitsMacro( ActivationFunction );
-        
+
       public:
         ActivationFunction( ) { }
         virtual ~ActivationFunction( ) = default;
-  
+
         virtual TMatrix operator()( const TMatrix& z ) = 0;
         virtual TMatrix operator[]( const TMatrix& z ) = 0;
       };
-      
+
       // -- Basic activation functions
       PUJ_NN_ActivationFunction( ArcTan );
       PUJ_NN_ActivationFunction( BinaryStep );
@@ -73,12 +72,12 @@ namespace PUJ
       PUJ_NN_ActivationFunction( Logistic );
       PUJ_NN_ActivationFunction( ReLU );
       PUJ_NN_ActivationFunction( SoftMax );
-      PUJ_NN_ActivationFunction( SoftMin );
+      PUJ_NN_ActivationFunction( SoftPlus );
       PUJ_NN_ActivationFunction( Tanh );
-      
+
       // -- Functions with parameters
-      PUJ_NN_ActivationFunction( ELU, Alpha, 1e-1 );
-      PUJ_NN_ActivationFunction( RandomReLU, Alpha, 1e-1 );
+      PUJ_NN_ActivationFunction_Parameter( ELU, Alpha, 1e-1 );
+      PUJ_NN_ActivationFunction_Parameter( RandomReLU, Alpha, 1e-1 );
 
     } // end namespace
   } // end namespace
