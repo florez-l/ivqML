b9492855d6ab00f984dbf89913159902537ec537
Leonardo Flórez-Valencia <florez-l@javeriana.edu.co>
2022-03-25 06:22:13 -0500

    Preparing branch

--- examples/02_logistic_regression/python3/test.m
+++
@@ -1,6 +0,0 @@
-X = rand( 1234, 2 );
-Y = rand( 1234, 1 );
-
-rows(X.*Y)
-columns(X.*Y)
-
--- lib/python3/PUJ/__init__.py
+++
@@ -1,8 +0,0 @@
-## =========================================================================
-## @author Leonardo Florez-Valencia (florez-l@javeriana.edu.co)
-## =========================================================================
-
-from .Model import *
-from .Optimizer import *
-
-## eof - $RCSfile$
