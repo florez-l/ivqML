93b3ec2e9ef1395fd003af6def45995e2b070500
Leonardo Flórez-Valencia <florez-l@javeriana.edu.co>
2023-10-13 11:02:09 -0500

    ...

--- lib/python3/PUJ/Model/__init__.py
+++
@@ -1,9 +0,0 @@
-## =========================================================================
-## @author Leonardo Florez-Valencia (florez-l@javeriana.edu.co)
-## =========================================================================
-
-from .Base import *
-from .Linear import *
-from .Logistic import *
-
-## eof - $RCSfile$
