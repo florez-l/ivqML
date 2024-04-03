4fb01bda45c4bb17c74b93757897b5d24e43fabe
Leonardo Flórez-Valencia <florez-l@javeriana.edu.co>
2024-04-03 16:34:22 -0500

    ...

--- examples/LinearModel_00.cxx
+++ examples/LinearModel_00.cxx
@@ -2,6 +2,7 @@
 // @author Leonardo Florez-Valencia (florez-l@javeriana.edu.co)
 // =========================================================================
 
+#include <chrono>
 #include <iostream>
 #include <ivqML/Model/Regression/Linear.h>
 #include <ivqML/Cost/MeanSquareError.h>
@@ -12,35 +13,75 @@ using TCost = ivqML::Cost::MeanSquareError< TModel >;
 
 int main( int argc, char** argv )
 {
+  auto ts = std::chrono::high_resolution_clock::now( );
+  auto te = std::chrono::high_resolution_clock::now( );
+
   unsigned int n = 4;
   unsigned int m = 10;
+  unsigned int r = 10;
+
+  if( argc > 1 ) n = std::atoi( argv[ 1 ] );
+  if( argc > 2 ) m = std::atoi( argv[ 2 ] );
+  if( argc > 3 ) r = std::atoi( argv[ 3 ] );
 
   // A model
+  TReal t = 0;
   TModel model( n );
-  model.random_fill( );
-  std::cout << "Model: " << model << std::endl;
+  for( unsigned int i = 0; i < r; ++i )
+  {
+    ts = std::chrono::high_resolution_clock::now( );
+    model.random_fill( );
+    te = std::chrono::high_resolution_clock::now( );
+    t +=
+      TReal(
+        std::chrono::duration_cast< std::chrono::nanoseconds >
+        ( te - ts ).count( )
+        ) / TReal( r );
+  } // end for
+  std::cout << "Model: " << model << " (" << t * 10e-9 << " s)" << std::endl;
 
   // Some random input data
   TModel::TMat X( n, m );
   X.setRandom( );
-  std::cout << "-------------- INPUTS --------------" << std::endl;
-  std::cout << X << std::endl;
 
-  std::cout << "-------------- OUTPUTS --------------" << std::endl;
-  TModel::TMat Y = model.eval( X );
-  std::cout << Y << std::endl;
+  // Evaluate from input data
+  TModel::TMat Y;
+  t = 0;
+  for( unsigned int i = 0; i < r; ++i )
+  {
+    ts = std::chrono::high_resolution_clock::now( );
+    Y = model.eval( X );
+    te = std::chrono::high_resolution_clock::now( );
+    t +=
+      TReal(
+        std::chrono::duration_cast< std::chrono::nanoseconds >
+        ( te - ts ).count( )
+        ) / TReal( r );
+  } // end for
+  std::cout << "Evaluate mean time: " << t * 10e-9 << " s" << std::endl;
 
-  std::cout << "-------------- COST --------------" << std::endl;
+  // New model for cost
   TModel model_for_cost( model.number_of_inputs( ) );
   model_for_cost.random_fill( );
 
+  // Cost model
   TCost J;
   J.set_data( X.data( ), Y.data( ), m, n, 1 );
-
   TModel::TRow G( model_for_cost.number_of_parameters( ) );
-  std::cout << "Model for cost: " << model_for_cost << std::endl;
-  std::cout << "Cost = " << J( model_for_cost, G.data( ) ) << std::endl;
-  std::cout << "Gradient = " << G << std::endl;
+
+  t = 0;
+  for( unsigned int i = 0; i < r; ++i )
+  {
+    ts = std::chrono::high_resolution_clock::now( );
+    J( model_for_cost, G.data( ) );
+    te = std::chrono::high_resolution_clock::now( );
+    t +=
+      TReal(
+        std::chrono::duration_cast< std::chrono::nanoseconds >
+        ( te - ts ).count( )
+        ) / TReal( r );
+  } // end for
+  std::cout << "Cost mean time: " << t * 10e-9 << " s" << std::endl;
 
   return( EXIT_SUCCESS );
 }
